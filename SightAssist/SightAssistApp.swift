6a0aeafa6c5715d9e3435a376bb94ffd3897e7d4
bucci369 <danyal64@hotmail.com>
2026-05-15 15:00:29 +0200

    Initial Commit

---
+++ SightAssist/SightAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightAssistApp.swift
+//  SightAssist
+//
+//  Created by Özgür Azap on 15.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SightAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SightAssistTests/SightAssistTests.swift
@@ -0,0 +1,18 @@
+//
+//  SightAssistTests.swift
+//  SightAssistTests
+//
+//  Created by Özgür Azap on 15.05.26.
+//
+
+import Testing
+
+struct SightAssistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
